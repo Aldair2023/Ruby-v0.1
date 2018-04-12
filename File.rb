80fbd669374327be1a53ea81d8e20acf0c0a8734
Aldair <ammc2023@gmail.com>
2018-04-12 12:15:35 -0500

    Paso tres

--- File.rb
+++ File.rb
@@ -1,5 +1,11 @@
+=begin
 puts "Hello World"
 
 age = 10
-
 puts "Usted tiene #{age} años"
+=end
+
+
+my_name= "Fill"
+puts "su nombre es #{my_name} y tiene #{(2**4) + 6} años"
+my_name = "gets.to_s"
